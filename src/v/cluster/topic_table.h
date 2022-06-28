c7442cebe72fc09c4babe0a1b11acb14e0df7a14
Michal Maslanka <michal@redpanda.com>
2022-06-28 09:43:31 +0200

    c/topics_table: expose apis to query partitions being moved
    Exposed APIs giving caller ability to query for all the partitions being
    moved in the cluster and partitions that are moving to/from a given
    node.
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>

--- src/v/cluster/topic_table.h
+++ src/v/cluster/topic_table.h
@@ -207,6 +207,18 @@ public:
         return _update_in_progress;
     }
 
+    /**
+     * Lists all NTPs that replicas are being move to a node
+     */
+    std::vector<model::ntp> ntps_moving_to_node(model::node_id) const;
+
+    /**
+     * Lists all NTPs that replicas are being move from a node
+     */
+    std::vector<model::ntp> ntps_moving_from_node(model::node_id) const;
+
+    std::vector<model::ntp> all_updates_in_progress() const;
+
 private:
     struct waiter {
         explicit waiter(uint64_t id)
